6613a48846a9886ac8dc817e54a4d1205cb31ca1
JxPV5 <169561139+JxPV5@users.noreply.github.com>
2026-03-29 22:59:32 +0200

    First

---
+++ script.js
@@ -0,0 +1,6 @@
+const posts = [
+    { title: "To my", date: "2026-03-28", image: "template.jpg", url: "posts/to-my.html" },
+    { title: "To my", date: "2026-03-28", image: "template.jpg", url: "posts/to-my.html" },
+    { title: "To my", date: "2026-03-28", image: "template.jpg", url: "posts/to-my.html" },
+    { title: "To my", date: "2026-03-28", image: "template.jpg", url: "posts/to-my.html" },
+];
