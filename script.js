ecc87320407679a52886bd08824e831c94319baa
JxPV5 <169561139+JxPV5@users.noreply.github.com>
2026-03-30 16:51:46 +0200

    How does this even work

--- script.js
+++
@@ -1,6 +0,0 @@
-const posts = [
-    { title: "To my", date: "2026-03-28", image: "template.jpg", url: "posts/to-my.html" },
-    { title: "To my", date: "2026-03-28", image: "template.jpg", url: "posts/to-my.html" },
-    { title: "To my", date: "2026-03-28", image: "template.jpg", url: "posts/to-my.html" },
-    { title: "To my", date: "2026-03-28", image: "template.jpg", url: "posts/to-my.html" },
-];
